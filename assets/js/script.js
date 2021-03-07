93be5d1a92f17a47ad503949eac8c96a7de4c26c
romaditro <sr.rdiazt@gmail.com>
2021-03-07 16:59:00 -0300

    Se agrego each, join y comentarios de codigo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,25 @@
-
 $(document).ready(function(){
 
-    const NUMERO_DE_POKEMONES = 10;
-    let idPokemon = "bulbasaur";
-
+    const NUMERO_DE_POKEMONES = 100;
     let url_api_pokemon = `https://pokeapi.co/api/v2/pokemon/?limit=${NUMERO_DE_POKEMONES}&offset=0`;
-    let url_api_pokemon_detalle = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
-  
-
     let poke_nombre;
 
-    //Grafico por defecto.
-
-    
+    //Grafico por defecto. 
     var radarData = {
       labels: ["HP", "Ataque", "Defensa", "Ataque Especial", "Defensa Especial", "Velocidad"],
       datasets: [
           {
               label: "",
-              fillColor: "#FF00BF",
-              strokeColor: "#FF00BF",
-              pointColor: "#FF00BF",
-              pointStrokeColor: "#FF00BF",
-              pointHighlightFill: "#E9CAD0",
-              pointHighlightStroke: "#E9CAD0",
-              data: [0,0,0,0,0,0]
+              // fillColor: "#FFE0E6",
+              // strokeColor: "#FFE0E6",
+              // pointColor: "#FFE0E6",
+              // pointStrokeColor: "#FFE0E6",
+              // pointHighlightFill: "#FFE0E6",
+              // pointHighlightStroke: "#FFE0E6",
+              borderCapStyle: 'butt',
+              borderColor: "#FF6384",
+              backgroundColor: "#FFE0E6",
+              data: null
           },
       ]
   };
@@ -34,12 +29,12 @@ $(document).ready(function(){
         angleShowLineOut : true,
         scaleShowLabels : false,
         scaleBeginAtZero : true,
-        angleLineColor : "#E9CAD0",
+        angleLineColor : "#FFE0E6",
         angleLineWidth : 1,
         pointLabelFontFamily : "'Arial'",
         pointLabelFontStyle : "normal",
         pointLabelFontSize : 10,
-        pointLabelFontColor : "#E9CAD0",
+        pointLabelFontColor : "#FFE0E6",
         pointDot : false,
         pointDotRadius : 2,
         pointDotStrokeWidth : 1,
@@ -66,7 +61,6 @@ $(document).ready(function(){
         //console.log(data.results);
         let pokemon = data.results;
         
-
         $('#select-pokeapi').append('<option value="0">Seleccione un Pokemon</option>');
         for(let i = 0; i < pokemon.length; i++){
           poke_nombre = pokemon[i].name.toUpperCase()
@@ -78,76 +72,71 @@ $(document).ready(function(){
 
 
 
-  //Capturar evento onchangue del select
-  
+  //Capturar evento onchangue del select  
   $("#select-pokeapi").on("change", function (v) {
     const selectedPoke = v.target.value;
-    //console.log("selectedPoke: " + selectedPoke);
+    let url_api_pokemon_detalle = `https://pokeapi.co/api/v2/pokemon/${selectedPoke}`;
 
     $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${selectedPoke}`,
+      url: url_api_pokemon_detalle,
       type: "GET",
       dataType: "JSON",
-      success: function (data) {       
-        const pokemonOrden = data.order;
-        $("#orden").text(pokemonOrden);
-
-        const spriteFront = data.sprites.back_default;
-
-        const artwork = data.sprites.other
+      success: function (data) {     
         
+        //Imagen
         let img_artwork = "";
- 
+        const artwork = data.sprites.other
+
         $.each(artwork, function(index, item) {
           img_artwork = item.front_default
         });
         $("#img_artwork").attr("src", img_artwork);
 
 
-        //Caracteristicas.
-        
-        //Tipo
-        let tipo = data.types[0].type.name;
-        //tipo += " - " + data.types[1].type.name;
+        //Orden        
+        const pokemonOrden = data.order;
+        $("#orden").text(pokemonOrden);
 
-        $("#tipo").text(tipo);
-
-
-        //Habilidades.
-        let habilidades = data.abilities[0].ability.name;
-        //habilidades += " - " + data.abilities[1].ability.name;
-
-        $("#habilidades").text(habilidades);
-
-
+        //Altura
         const pokemonAltura = data.height;
         $("#altura").text(pokemonAltura);
 
+
+        //Peso
         const pokemonPeso = data.weight;
         $("#peso").text(pokemonPeso);
 
 
+        //Tipo
+        let tipo = [];
+        $.each(data.types, function(index, item) {
+          tipo.push(item.type.name);
+        });
+
+        $("#tipo").text(tipo.join(" / "));
+
+
+        //Habilidades.
+        let habilidades =[];
+
+        $.each(data.abilities, function(index, item) {
+          habilidades.push(item.ability.name);
+        });
+        
+        $("#habilidades").text(habilidades.join(" / "));
 
         //Grafico
         let estadisticas = [];
 
-        estadisticas.push(parseInt(data.stats[0].base_stat)); 
-        estadisticas.push(parseInt(data.stats[1].base_stat)); 
-        estadisticas.push(parseInt(data.stats[2].base_stat)); 
-        estadisticas.push(parseInt(data.stats[3].base_stat)); 
-        estadisticas.push(parseInt(data.stats[4].base_stat)); 
-        estadisticas.push(parseInt(data.stats[5].base_stat)); 
+        $.each(data.stats, function(index, item) {
+          estadisticas.push(item.base_stat);
+        });
 
-        console.log(estadisticas);
-
+        //Actualizar el grafico.
         myRadarChart.data.datasets[0].data =  estadisticas;
         myRadarChart.update();
 
       }
     });
   });
-
-
-
 });
-
